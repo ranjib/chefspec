e3dc726be578fd127a254d9ede1deb836891a731
Ranjib Dey <ranjib@pagerduty.com>
2013-05-14 16:46:06 -0700

    fix syntax

--- lib/chefspec/stub.rb
+++ lib/chefspec/stub.rb
@@ -1,12 +1,12 @@
 class ChefSpec
   class Stub
     def self.search(&super_block)
-      class Chef::Recipe.class_eval do
+      Chef::Recipe.class_eval do
         def search(*args, &block)
           super_block.call(*args)
         end
       end
-      class Chef::Resource.class_eval do
+      Chef::Resource.class_eval do
         def search(*args, &block)
           super_block.call(*args)
         end
